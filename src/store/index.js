f1b000ec868c5f497ccdb32135461e2a89d6c61e
Tony Chung <waiting33118@gmail.com>
2021-05-01 21:31:44 +0800

    feat: edit vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,32 @@
-import { createStore } from 'vuex';
+import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    username: '',
+    mediaStream: {}
   },
   mutations: {
+    setMediaStream (state, mediaStream) {
+      state.mediaStream = mediaStream
+    },
+    setUsername (state, username) {
+      state.username = username
+    }
   },
   actions: {
+    setMediaStream ({ commit }, mediaStream) {
+      commit('setMediaStream', mediaStream)
+    },
+    setUsername ({ commit }, username) {
+      commit('setUsername', username)
+    }
   },
-  modules: {
-  },
-});
+  getters: {
+    getUsername (state) {
+      return state.username
+    },
+    getMediaStream (state) {
+      return state.mediaStream
+    }
+  }
+})
